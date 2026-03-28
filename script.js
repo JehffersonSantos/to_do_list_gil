1bec82c595697f82dbbcb22546bd7bad4154aa10
JehffersonSantos <jefersonsantos10@hotmail.com>
2026-03-28 01:00:55 -0300

    terminando a versão desktop

---
+++ script.js
@@ -0,0 +1,22 @@
+const botao = document.querySelector(".incluir")
+const incluirTarefaMobile = document.querySelector(".criar_tarefa_botão")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+botao.addEventListener('click', () => {
+    incluirTarefaMobile.classList.toggle("desativado")
+}
+
+)
